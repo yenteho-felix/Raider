5814cd535b0b1f55ecf595d124be3b554075425e
yenteho-felix <84868780+yenteho-felix@users.noreply.github.com>
2025-02-19 14:29:39 -0800

    Update comments

--- Raider/Source/Raider/Public/Share/MyCombatComponent.h
+++ Raider/Source/Raider/Public/Share/MyCombatComponent.h
@@ -20,7 +20,7 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAttackEnd);
 
 /**
  *  =====================================================
- *  Combat component to hold combat related properties and functions
+ *  Combat component responsible for handling combat mechanics
  *  =====================================================
  */
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -29,11 +29,9 @@ class RAIDER_API UMyCombatComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
 	UMyCombatComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 /**
@@ -42,37 +40,57 @@ protected:
  *  ---------------------------------------------
  */
 public:
+	/** Whether the NPC currently has a weapon equipped */
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Combat|Weapon")
 	bool IsWeaponEquipped;
 
+	/** The weapon class that can be spawned for the NPC */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|Weapon")
 	TSubclassOf<AWeaponBase> WeaponActorClass;
 
+	/** Reference to the currently equipped weapon */
 	UPROPERTY(BlueprintReadOnly, Category = "Combat|Weapon")
 	TObjectPtr<AWeaponBase> WeaponActorObj;
 
+	/** Animation montage played when equipping a weapon */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|Weapon")
 	TObjectPtr<UAnimMontage> EquipMontage;
-	
+
+	/** Animation montage played when unequipping a weapon */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|Weapon")
 	TObjectPtr<UAnimMontage> UnEquipMontage;
 
+	/** The socket name where the weapon will be attached on the character */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|Weapon")
 	FName WeaponSocketName;
 
+	/** Equips the assigned weapon */
 	UFUNCTION(BlueprintCallable, Category = "Combat|Weapon")
 	void EquipWeapon();
-	
+
+	/** Unequips the currently equipped weapon */
 	UFUNCTION(BlueprintCallable, Category = "Combat|Weapon")
 	void UnEquipWeapon();
 
 protected:
+	/**
+	 *  Attaches the weapon to a specified socket on the character.
+	 *  @param WeaponActor - The weapon actor to attach.
+	 */
 	UFUNCTION()
 	void AttachWeaponToSocket(AActor* WeaponActor) const;
 
+	/**
+	 *	Plays the equip montage animation.
+	 *	@param AnimMontage - The montage to play for equipping the weapon.
+	 */
 	UFUNCTION()
 	void PlayEquipMontage(UAnimMontage* AnimMontage);
 
+	/**
+	 *  Plays the unequip montage animation.
+	 *  @param AnimMontage - The montage to play for unequipping the weapon.
+	 */
 	UFUNCTION()
 	void PlayUnEquipMontage(UAnimMontage* AnimMontage);
 	
@@ -86,12 +104,20 @@ public:
 	FOnUnEquipWeaponEnd OnUnEquipWeaponEnd;
 
 protected:
-	/** Callback when OnEquipMontageEnded finishes to broadcast the OnEquipWeaponEnd event */
-	UFUNCTION()
+	/**
+	 *  Callback function triggered when the equip montage animation finishes.
+	 *  Broadcasts the `OnEquipWeaponEnd` event.
+	 *  @param Montage - The animation montage that completed.
+	 *  @param bInterrupted - Whether the montage was interrupted.
+	 */	UFUNCTION()
 	void OnEquipMontageEnded(UAnimMontage* Montage, bool bInterrupted) const;
 
-	/** Callback when OnUnEquipMontageEnded finishes to broadcast the OnUnEquipWeaponEnd event */
-	UFUNCTION()
+	/**
+	 *  Callback function triggered when the unequip montage animation finishes.
+	 *  Broadcasts the `OnUnEquipWeaponEnd` event.
+	 *  @param Montage - The animation montage that completed.
+	 *  @param bInterrupted - Whether the montage was interrupted.
+	 */	UFUNCTION()
 	void OnUnEquipMontageEnded(UAnimMontage* Montage, bool bInterrupted) const;
 
 /**
@@ -100,22 +126,35 @@ protected:
  *  ---------------------------------------------
  */
 public:
+	/** Animation montage played when the NPC performs an attack */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|Attack")
 	TObjectPtr<UAnimMontage> AttackMontage;
-	
+
+	/** The radius within which the NPC can perform an attack */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|Attack")
 	float AttackRadius;
 
+	/** The radius within which the NPC considers defending */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|Attack")
 	float DefendRadius;
-	
+
+	/**
+	 *  Retrieves the attack and defense ranges of the NPC.
+	 *  @param OutAttackRadius - Output parameter for attack radius.
+	 *  @param OutDefendRadius - Output parameter for defend radius.
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Combat|Attack")
 	void GetCombatRange(float& OutAttackRadius, float& OutDefendRadius) const;
 
+	/** Executes the NPC's attack behavior */
 	UFUNCTION(BlueprintCallable, Category = "Combat|Attack")
 	void Attack();
 
 protected:
+	/**
+	 *  Plays the attack montage animation.
+	 *  @param AnimMontage - The montage to play for attacking.
+	 */
 	UFUNCTION()
 	void PlayAttackMontage(UAnimMontage* AnimMontage);
 	
@@ -124,7 +163,11 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "Combat|Attack")
 	FOnAttackEnd OnAttackEnd;
 	
-	/** Callback when OnUnAttackMontageEnded finishes to broadcast the OnAttackEnd event */
-	UFUNCTION(BlueprintCallable, Category = "Combat|Attack")
+	/**
+	 *  Callback function triggered when the attack montage animation finishes.
+	 *  Broadcasts the `OnAttackEnd` event.
+	 *  @param Montage - The animation montage that completed.
+	 *  @param bInterrupted - Whether the montage was interrupted.
+	 */	UFUNCTION(BlueprintCallable, Category = "Combat|Attack")
 	void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted) const;
 };
--- Raider/Source/Raider/Public/Share/MyHealthComponent.h
+++ Raider/Source/Raider/Public/Share/MyHealthComponent.h
@@ -6,33 +6,49 @@
 #include "Components/ActorComponent.h"
 #include "MyHealthComponent.generated.h"
 
-
+/**
+ *  =====================================================
+ *  A health management component that handles health-related functionalities
+ *  =====================================================
+ */
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class RAIDER_API UMyHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
 	UMyHealthComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
+	/** Current health of the entity */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
 	float Health;
 
+	/** Maximum health of the entity */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
 	float MaxHealth;
 
+	/**
+	 * Restores health to the entity.
+	 * @param HealAmount - The amount of health to restore.
+	 */
 	UFUNCTION(BlueprintCallable)
 	void TakeHealing(float HealAmount);
-	
+
+	/**
+	 * Reduces the entity's health when taking damage.
+	 * @param DamageAmount - The amount of damage to apply.
+	 */
 	UFUNCTION(BlueprintCallable)
 	void TakeDamage(float DamageAmount);
 
+	/**
+	 * Checks if the entity is still alive.
+	 * @return True if health is above zero, otherwise false.
+	 */
 	UFUNCTION(BlueprintCallable)
 	bool IsAlive() const;
 };
